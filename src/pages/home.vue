8e1ef38b02b4c58f213d418d23f50889a6f437ac
Corina Pelichowski <corina@danitt.com>
2021-11-09 23:42:18 +1000

    verbs layout = complete

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -15,76 +15,11 @@
   </div>
 </template>
 <script>
+import homeJson from "./home.json";
 export default {
   data() {
     return {
-      dom: [
-        {
-          english: "Alphabet",
-          polish: "Alfabet",
-          image: "/home/alphabet.jpg",
-        },
-        {
-          english: "Animals",
-          polish: "Zwierzę",
-          image: "/home/animals.jpg",
-        },
-        {
-          english: "Colours",
-          polish: "Kolory",
-          image: "/home/colours.jpg",
-        },
-        {
-          english: "Drinks",
-          polish: "Napoje",
-          image: "/home/drinks.jpg",
-        },
-        {
-          english: "Food",
-          polish: "Jedzenie",
-          image: "/home/food.jpg",
-        },
-        {
-          english: "Hobbies",
-          polish: "Zainteresowania",
-          image: "/home/hobbies.jpg",
-        },
-        {
-          english: "Numbers",
-          polish: "Liczby",
-          image: "/home/numbers.jpg",
-        },
-        {
-          english: "Occupations",
-          polish: "Zawody",
-          image: "/home/occupations.jpg",
-        },
-        {
-          english: "People",
-          polish: "Ludzie",
-          image: "/home/people.jpg",
-        },
-        {
-          english: "Time",
-          polish: "Czas",
-          image: "/home/time.jpg",
-        },
-        {
-          english: "Transport",
-          polish: "Transport",
-          image: "/home/transport.jpg",
-        },
-        {
-          english: "Verbs",
-          polish: "Czasowniki",
-          image: "/home/verbs.jpg",
-        },
-        {
-          english: "Weather",
-          polish: "Pogoda",
-          image: "/home/weather.jpg",
-        },
-      ],
+      dom: homeJson,
     };
   },
 };
---
+++ src/pages/verbs.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card v-for="verbs in czasowniki" :key="verbs.english">
+      <q-img class="card-img" :src="verbs.image" />
+      <table class="q-table">
+        <tbody>
+          <tr>
+            <td>{{ verbs.english }}</td>
+            <td>{{ verbs.polish }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </q-card>
+  </div>
+</template>
+<script>
+import verbsJson from "./verbs.json";
+export default {
+  data() {
+    return {
+      czasowniki: verbsJson,
+    };
+  },
+};
+</script>

a46f4f4f3577669d01f3730d0f62b2db6d23475c
Corina Pelichowski <corina@danitt.com>
2021-11-16 20:01:17 +1000

    changed images to original and created nested page for bread

---
+++ src/pages/food/food.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="q-pa-md row items-start q-gutter-md">
+    <q-card v-for="food in jedzenie" :key="food.english">
+      <router-link :to="food.link">
+        <q-img class="card-img" :src="food.image" />
+        <table class="q-table">
+          <tbody>
+            <tr>
+              <td>{{ food.english }}</td>
+              <td>{{ food.polish }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </router-link>
+    </q-card>
+  </div>
+</template>
+<script>
+import foodJson from "./food.json";
+export default {
+  data() {
+    return {
+      jedzenie: foodJson,
+    };
+  },
+};
+</script>

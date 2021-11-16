555f89fb4b1bc965945fca6a825b379b410f0c68
Corina Pelichowski <corina@danitt.com>
2021-11-16 10:47:06 +1000

    home cards links to other pages now working

--- src/pages/home.vue
+++ src/pages/home.vue
@@ -1,16 +1,17 @@
 <template>
   <div class="q-pa-md row items-start q-gutter-md">
     <q-card v-for="home in dom" :key="home.english">
-      <q-img class="card-img" :src="home.image" />
-      <table class="q-table">
-        <!-- <q-item to="{{ colour.href }}" exact clickable> {{}} </q-item> this may be the link but doesn't quite work-->
-        <tbody>
-          <tr>
-            <td>{{ home.english }}</td>
-            <td>{{ home.polish }}</td>
-          </tr>
-        </tbody>
-      </table>
+      <router-link :to="home.link">
+        <q-img class="card-img" :src="home.image" />
+        <table class="q-table">
+          <tbody>
+            <tr>
+              <td>{{ home.english }}</td>
+              <td>{{ home.polish }}</td>
+            </tr>
+          </tbody>
+        </table>
+      </router-link>
     </q-card>
   </div>
 </template>

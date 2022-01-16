544e71adaf470b217773b63370a106de9ca5e070
Corina Pelichowski <corina@danitt.com>
2022-01-16 18:39:28 +1000

    verbs

--- src/pages/verbs.vue
+++ src/pages/verbs.vue
@@ -1,12 +1,26 @@
 <template>
   <div class="q-pa-md row items-start q-gutter-md">
-    <q-card v-for="verbs in czasowniki" :key="verbs.english">
-      <q-img class="card-img" :src="verbs.image" />
+    <q-card v-for="verbs in czasowniki" :key="verbs.ja">
       <table class="q-table">
+        <th>{{ verbs.title }}</th>
         <tbody>
           <tr>
-            <td>{{ verbs.english }}</td>
-            <td>{{ verbs.polish }}</td>
+            <td>{{ verbs.ja }}</td>
+          </tr>
+          <tr>
+            <td>{{ verbs.ty }}</td>
+          </tr>
+          <tr>
+            <td>{{ verbs.on }}</td>
+          </tr>
+          <tr>
+            <td>{{ verbs.my }}</td>
+          </tr>
+          <tr>
+            <td>{{ verbs.wy }}</td>
+          </tr>
+          <tr>
+            <td>{{ verbs.oni }}</td>
           </tr>
         </tbody>
       </table>
